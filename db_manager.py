cae785cc386da6238583ec64604f89fd2325c931
Ács Árpád <alijie24@gmail.com>
2024-03-14 20:31:16 +0100

    create registration and login function

---
+++ db_manager.py
@@ -0,0 +1,55 @@
+import psycopg2
+from config import *
+from datetime import datetime
+from customtkinter import *
+from config import DatabaseManager
+
+
+database_manager = DatabaseManager()
+
+def registration(name, uid, email, password, message, win):
+    try:
+        if not name.get() and not uid.get() and not email.get() and not password.get():
+            win.destroy()
+            message.showerror(title="Registration error", message="Fields are mandatory")
+            return
+
+        database_manager.curs.execute("INSERT INTO registration (user_company_id, email_address, password, name, class) VALUES (?, ?, ?, ?, ?)", (uid.get(), email.get(), password.get(), name.get(), "F",))
+        database_manager.conn.commit()
+        name.delete(0, 'end')
+        uid.delete(0, 'end')
+        email.delete(0, 'end')
+        password.delete(0, 'end')
+        message.showinfo(title="Registration", message="Successfully registered! You can now log in!")
+        win.destroy()
+    except psycopg2.Error as e:
+        message.showinfo(title="Failed to registration", message=f"Failed in registration, PostgreSQL error: {e}")
+        win.destroy()
+        return
+    
+def login(email, password, win):
+    today = datetime.now()
+    login_label = CTkLabel(win, text="", font=("Arial", 14, "bold"))
+    login_label.place(x=320, y=440)
+    
+    if not email.get() and not password.get():
+            login_label.configure(text="Field is mandantory!", text_color=("#f09999"))
+            return
+
+    database_manager.curs.execute("SELECT * FROM registration WHERE email_address = ? AND password = ?", (email.get(), password.get(),))
+    datas = database_manager.curs.fetchall()
+    
+    if  datas:
+        myValc = IntVar()
+        progressbar = CTkProgressBar(win, orientation="horizontal", width=150, height=15, mode="determinate", determinate_speed=1, variable=myValc)
+        progressbar.place(x=320, y=420)
+        login_label.configure(text=f"{database_manager.connecting}", font=("Arial", 12))
+
+        database_manager.curs.execute("UPDATE registration SET permission = true, last_login = ? WHERE email_address = ?", (today, email.get(),))
+        database_manager.conn.commit()
+        win.destroy()
+    else:
+        login_label.configure(text="Invalid login.", text_color=("#f09999"))
+        #message.showerror(title="Error", message="Invalid login.")
+        email.delete(0, 'end')
+        password.delete(0, 'end')
---
+++ reg.py
@@ -0,0 +1,34 @@
+from customtkinter import *
+from tkinter import messagebox
+from db_manager import *
+
+def user_reg():
+    log = CTk()
+    log.title("Registration")
+    log.geometry('500x500')
+    set_appearance_mode("light")
+    log.resizable(False, False)
+
+    reg_label = CTkLabel(log, text="Registration", font=("Arial", 30))
+    name_label = CTkLabel(log, text="Name", font=("Arial", 16, 'bold'))
+    reg_id_label = CTkLabel(log, text="User ID", font=("Arial", 16, 'bold'))
+    reg_email_label = CTkLabel(log, text="Email", font=("Arial", 16, 'bold'))
+    reg_password_label = CTkLabel(log, text="Password", font=("Arial", 16, 'bold'))
+    reg_name_entry = CTkEntry(log, font=("Arial", 16))
+    reg_id_entry = CTkEntry(log, font=("Arial", 16))
+    reg_email_entry = CTkEntry(log, font=("Arial", 16))
+    reg_password_entry = CTkEntry(log, show="*", font=("Arial", 16))
+    reg_button = CTkButton(log, text="Registration", font=("Arial", 16), command=lambda: registration(reg_name_entry, reg_id_entry, reg_email_entry, reg_password_entry, messagebox, log))
+
+    reg_label.place(relx=0.5, rely=0.2, anchor="center")
+    name_label.place(relx=0.3, rely=0.3, anchor="center")
+    reg_id_label.place(relx=0.3, rely=0.4, anchor="center")
+    reg_email_label.place(relx=0.3, rely=0.5, anchor="center")
+    reg_password_label.place(relx=0.3, rely=0.6, anchor="center")
+    reg_name_entry.place(relx=0.6, rely=0.3, anchor="center")
+    reg_id_entry.place(relx=0.6, rely=0.4, anchor="center")
+    reg_email_entry.place(relx=0.6, rely=0.5, anchor="center")
+    reg_password_entry.place(relx=0.6, rely=0.6, anchor="center")
+    reg_button.place(relx=0.5, rely=0.7, anchor="center")
+
+    log.mainloop()

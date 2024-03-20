6f13bc2f93971797f9a586a3e3c68092e3ac15ff
Ács Árpád <alijie24@gmail.com>
2024-03-20 21:28:50 +0100

    delete_data(), del_rows(), user_card(), time()

--- save_data.py
+++ save_data.py
@@ -4,15 +4,18 @@ from PIL import ImageTk, Image
 from tkcalendar import DateEntry
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
+from datetime import datetime, timedelta
+from time import strftime
 
-
+database_manager = DatabaseManager()
+#Globális változók
 add_data_win = None
 table = None
 add_data_frame = None
 get_fetched_id = None
 get_fetched_name = None
 Class = None
-database_manager = DatabaseManager()
 my_tag = 'normal'
 
 today_date = None
@@ -30,10 +33,11 @@ def save_data():
   add_data_win.geometry('1400x850')
   set_appearance_mode("light")
   add_data_win.resizable(False, False)
+  user_card()
   #Menu
   mainmenu  = tk.Menu(add_data_win)
   file_menu = tk.Menu(mainmenu, tearoff=0)
-  file_menu.add_command(label="Exit",font="Helvetica 8 bold")
+  file_menu.add_command(label="Exit",font="Helvetica 8 bold", command=lambda: logout(add_data_win))
   
   mainmenu.add_cascade(label="Menu", menu=file_menu, font="Helvetica 10")
   add_data_win.config(menu=mainmenu)
@@ -68,8 +72,12 @@ def save_data():
   comment_textBox = CTkTextbox(add_data_frame, width=250, height=30, border_width=1)
   #Button
   
-  delete_all_button = CTkButton(add_data_frame, text="Delete all")
-  add_button = CTkButton(add_data_frame, text="Add")
+  delete_all_button = CTkButton(add_data_frame, text="Delete all", command=lambda: delete_data( 
+                                                                              calendar_start_entry, calendar_end_entry,start_hour, start_min, end_hour, end_min, 
+                                                                              reason_textBox,comment_textBox))
+  add_button = CTkButton(add_data_frame, text="Add", command=lambda: add_data(group_omenu, 
+                                                                              calendar_start_entry, calendar_end_entry,start_hour, start_min, end_hour, end_min, 
+                                                                              month_omenu, type_omenu, reason_textBox,comment_textBox))
   if Class == "A":
     file_menu.add_separator()
     file_menu.add_command(label="Edit profile",font="Helvetica 8 bold")
@@ -119,7 +127,7 @@ def save_data():
   table_frame = CTkFrame(add_data_win)
   table_frame.place(x=5, y=400)
   #add coll title
-  cols = ("ID", "Name", "Group", "Start", "End", "Month", "Type", "Reason", "Comment", "Negative time", "Counted Time", "Approval")
+  cols = ("Name", "Group", "Start", "End", "Month", "Type", "Reason", "Comment", "Negative time", "Counted Time", "Approval")
   table = ttk.Treeview(table_frame, columns=cols, height=20)
   scrollbar = CTkScrollbar(table_frame, command=table.yview)
   
@@ -132,6 +140,9 @@ def save_data():
   table.pack(side="left", fill="y")
   table.configure(yscrollcommand=scrollbar.set)
   scrollbar.pack(side="right", fill="y")
+  delete_data(calendar_start_entry, calendar_end_entry,start_hour, start_min, end_hour, end_min,reason_textBox,comment_textBox)
+  get_data(table)
+  time()
   add_data_win.mainloop()
 
 def add_data(group, start, end, strhour, strmin, ehour, emin, month, type, reason, comment ):
@@ -142,16 +153,10 @@ def add_data(group, start, end, strhour, strmin, ehour, emin, month, type, reaso
     #--------------------------------------------------------------------------
     
     #Konvertálom dátum és idő objektummá, algoritmus a számlált napokra
-    
-    start_date_to_postgresql = datetime.strptime(start.get(), "%m/%d/%y")    #%m/%d/%y
-    
-    end_date_to_postgresql = datetime.strptime(end.get(), "%m/%d/%y")        #"%Y. %m. %d."
-
-    
+    start_date_to_postgresql = datetime.strptime(start.get(), "%Y. %m. %d.")    #%m/%d/%y       -> legenerált exe-vel
+    end_date_to_postgresql = datetime.strptime(end.get(), "%Y. %m. %d.")        #"%Y. %m. %d."  -> vscode-ban
     day_diff = end_date_to_postgresql - start_date_to_postgresql
-
     hours_diff = int(ehour.get()) - int(strhour.get())
-    
     min_diff = int(emin.get()) - int(strmin.get())
 
     #--------------------------------------------------------------------------
@@ -168,17 +173,13 @@ def add_data(group, start, end, strhour, strmin, ehour, emin, month, type, reaso
 
     #--------------------------------------------------------------------------
     
-    # máshogy printelem a napokat
-        
+    # máshogy printelem a napokat 
     diff = f"{day_diff.days} day(s) {hours_diff} hour {min_diff} minutes"
-
-    
     database_manager.curs.execute("INSERT INTO insertdata (user_company_id, name, team_group, month, type, start_date, end_date, start_hour, start_min, end_hour, end_min, reason, comment, counted_day, counted_hour, counted_min, counted_time, approval) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
                  (get_fetched_id, get_fetched_name, group.get(), month.get(), type.get(), start_date_to_postgresql.strftime("%Y-%m-%d"), end_date_to_postgresql.strftime("%Y-%m-%d"), strhour.get(), strmin.get(), ehour.get(), emin.get(), reason.get("0.0", "end"), comment.get("0.0", "end"), int(day_diff.days), hours_diff, min_diff, diff, "false"))
     database_manager.conn.commit()
 
-    # delete_data(start, end, strhour, strmin, ehour, emin, reason, comment)
-    
+    delete_data(start, end, strhour, strmin, ehour, emin, reason, comment)
     get_data(table)
 
 #--------------------------------------------------------------------------
@@ -189,24 +190,62 @@ def get_data(table):
     #--------------------------------------------------------------------------   
    
     # Query csak egy főre
-
     query_one_user = "SELECT insertdata.* FROM insertdata JOIN registration ON insertdata.user_company_id = registration.user_company_id WHERE registration.user_company_id = :user_id"
     params_one_user = {"user_id": get_fetched_id}
     
     #--------------------------------------------------------------------------
    
     # Query auth level A-val az összes főre
-    
     database_manager.curs.execute(query_all if Class == 'A' else query_one_user, params_one_user)
     datas = database_manager.curs.fetchall()
     table.delete(*table.get_children())
 
-    #insert_data(datas)
+    insert_data(datas)
     #chart_frame(add_data_win, get_fetched_id)
-    #user_card()
+    user_card()
 
 #--------------------------------------------------------------------------
 
+def user_card():
+    global get_fetched_id, get_fetched_name, Class, today_date, overtime, standby, sliding
+    database_manager.curs.execute('''SELECT registration.user_company_id, 
+                                                registration.name, registration.class, 
+                                                registration.last_login, 
+                                                COALESCE(COUNT(CASE WHEN insertdata.type = 'Túlóra' THEN 1 END),0), 
+                                                COALESCE(COUNT(CASE WHEN insertdata.type = 'Készenlét' THEN 1 END),0), 
+                                                COALESCE(COUNT(CASE WHEN insertdata.type = 'Csúszó' THEN 1 END),0) 
+                                        FROM registration 
+                                        LEFT JOIN insertdata ON registration.user_company_id = insertdata.user_company_id 
+                                        WHERE registration.permission = true 
+                                        GROUP BY registration.user_company_id, registration.name, registration.class''')
+    user_data = database_manager.curs.fetchall()
+
+    for data in user_data:
+        get_fetched_id = data[0]
+        get_fetched_name = data[1]
+        Class = data[2]
+        today_date = data[3]#.strftime("%Y-%m-%d %H:%M")
+        overtime = data[4]
+        standby = data[5]
+        sliding = data[6]
+    #Card Label
+    CIAM_label= CTkLabel(add_data_frame, text=f"CIAM ID: {get_fetched_id}", font=("Arial", 16, 'bold'))
+    name_label= CTkLabel(add_data_frame, text=f"NAME: {get_fetched_name}", font=("Arial", 16, 'bold'))
+    llogin_label= CTkLabel(add_data_frame, text=f"Last login: {today_date}", font=("Arial", 16, 'bold'))
+    authL_label= CTkLabel(add_data_frame, text=f"authorization level: {Class}", font=("Arial", 16, 'bold'))
+    overtime_label= CTkLabel(add_data_frame, text=f"Overtime: {overtime}", font=("Arial", 16, 'bold'))
+    standby_label= CTkLabel(add_data_frame, text=f"Standby: {standby}", font=("Arial", 16, 'bold'))
+    sliding_label= CTkLabel(add_data_frame, text=f"Sliding: {sliding}", font=("Arial", 16, 'bold'))
+    CIAM_label.place(x=460, y=10)
+    name_label.place(x=630, y=10)
+    llogin_label.place(x=460, y=40)
+    authL_label.place(x=460, y=70)
+    overtime_label.place(x=460, y=100)
+    standby_label.place(x=460, y=130)
+    sliding_label.place(x=460, y=160)
+
+#--------------------------------------------------------------------------
+    
 def insert_data(datas):
     for i, data in enumerate(datas):
         clean_data = ["" if d is None else d for d in data]
@@ -218,10 +257,50 @@ def insert_data(datas):
         ), tags=('unchecked', my_tag))
 
 #--------------------------------------------------------------------------
+        #Input mező törlés
+def delete_data(start, end, strhour, strmin, ehour, emin, reason, comment ):
+    start.delete(0, 'end')
+    end.delete(0, 'end')
+    strhour.delete(0, 'end')
+    strmin.delete(0, 'end')
+    ehour.delete(0, 'end')
+    emin.delete(0, 'end')
+    reason.delete("0.0", "end")
+    comment.delete("0.0", "end")
 
+
+#--------------------------------------------------------------------------
+def time():
+  stringtime = strftime('%H:%M:? %p')
+  clock.configure(text=stringtime)
+  clock.after(1000,time)
+
+
+#--------------------------------------------------------------------------
+    #Rekord törlés a DB-ből valamint a Treeview-ból is
+def del_rows():
+    sel_row = CTkLabel(add_data_win, text="")
+    sel_row.place(x=630, y=335)
+    selected_rows = table.selection()
+    if len(selected_rows) == 0:
+        sel_row.configure(text="No rows selected", text_color=("#f09999"), font=("Arial", 18))
+    else:
+        for rowid in selected_rows:
+            tags = table.item(rowid, "tags")
+            if "checked" in tags and Class == "A":
+                get_row = table.item(rowid, "values")[0]
+                
+                database_manager.curs.execute("DELETE FROM insertdata WHERE row_id = ?", (get_row,))
+                database_manager.conn.commit()
+            else:
+                table.item(rowid, tags=('unchecked', my_tag))
+                return
+    get_data(table)
+    
+    
 def logout(win):
      database_manager.curs.execute("UPDATE registration SET permission = false WHERE user_company_id = ?", (get_fetched_id,))
      database_manager.conn.commit()
      win.destroy()
 
-#save_data()
+save_data()

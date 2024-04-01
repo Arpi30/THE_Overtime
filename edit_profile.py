457f406f1794ad7009382e3ebc74bd7c24450ac0
Ács Árpád <alijie24@gmail.com>
2024-04-01 17:43:39 +0200

    edit_profile comments

--- edit_profile.py
+++ edit_profile.py
@@ -6,10 +6,9 @@ database_manager = DatabaseManager()
 edit_profile_table = None
 
 def edit_profile(fetched_id):
-
-    #profil szerkesztő ablak ablak
-
+    #globális változó
     global edit_profile_table
+    #profil szerkesztő ablak inicializálása: focus,cím,méret,mód
     edit = CTkToplevel()
     edit.grab_set()
     edit.title("Edit profile")
@@ -18,29 +17,27 @@ def edit_profile(fetched_id):
     edit.resizable(False, False)
 
     #edit gomb hozzáadása a frame-hez
-    
     edit_profile_table_frame = CTkFrame(edit)
     edit_profile_table_frame.place(x=3, y=50)
     
-    #oszlop címezése
-    
+    #oszlopok hozzáadása a Treeview-hoz
     cols = ("Name", "ID", "Class")
     edit_profile_table = ttk.Treeview(edit_profile_table_frame, columns=cols, height=12)
     scrollbar = CTkScrollbar(edit_profile_table_frame, command=edit_profile_table.yview)
 
     #oszlopok elhelyezése, berendezése 
-    
     for col in cols:
       edit_profile_table.heading(col, text=col, anchor='center')
       edit_profile_table.column(col, width=154, anchor="center")
       edit_profile_table.column("#0", width=28, anchor="ne")
     edit_profile_table.pack(side="left", fill="y")
     edit_profile_table.configure(yscrollcommand=scrollbar.set)
+    #Adatok lekérése db-ből
     get_data()    
-
+    #Felhasználó törlése gomb
     del_member = CTkButton(edit, text="Delete member", command=lambda:delete_member(fetched_id, edit))
     del_member.place(x=190, y=350)
-
+    #Felhasználói adaotk módosítása esemény
     edit.bind("<Double-Button-1>", lambda event: update_profile(event, edit_profile_table))
 
 
@@ -50,41 +47,39 @@ def edit_profile(fetched_id):
 
 def get_data():
    
-   #sql query: fetch, adat kijelölése
-   
+   #Adatok lekérése db-ből
    database_manager.curs.execute("SELECT name, user_company_id, class FROM registration")
    datas = database_manager.curs.fetchall()
+   #Minden lekérésnél törölni kell a Treeview-t
    edit_profile_table.delete(*edit_profile_table.get_children())
 
-   #adatbázis hiányos adatainak feltöltése NULL-al
-
+   #Lekért adatok végig iterálása és elhelyezése a Treeview-ban
    for i, data in enumerate(datas):
     clean_data = ["" if d is None else d for d in data]
     edit_profile_table.insert("", "end",values=(clean_data[0], clean_data[1], clean_data[2]))
 
 
 def delete_member(fetched_id, win):
-
-    #tag törlés függvény
-
+    #Felhasználó kiválasztása
    notapproved_delete_member = CTkLabel(win, text="", font=("Arial", 14, "bold"))
    notapproved_delete_member.pack()
    item = edit_profile_table.selection()
    values = edit_profile_table.item(item, "values")
  
-   #sql sor törlése és adatbázis frissítése
+   #Felhasználó törlése a db-ből
    database_manager.curs.execute("DELETE FROM registration WHERE user_company_id = ? AND NOT user_company_id= ?", (values[1], fetched_id,))
    database_manager.conn.commit()
    
-   #hibakezelés arra, ha bizonyos tagokat engedély nélkül akarnak törölni
-
+   #A felhasználó nem törölheti saját magát
    if fetched_id == values[1]:
       notapproved_delete_member.configure(text="This member cannot be deleted!", text_color=("#f09999"))
+   #Adatok lekérése db-ből
    get_data()
 
 def update_profile(event, table):
+   #Cella kijelölése
    region_clicked = table.identify_region(event.x, event.y)
-   
+   #Hibakezelése ha a cella nem "cell" típusú hanem pl.: header
    if region_clicked not in ("cell"):
       return
    
@@ -111,6 +106,7 @@ def update_profile(event, table):
    entry_edit.bind("<Return>", lambda event: enter_pressed(event, table, selected_values))
 
 def focus_out(event):
+   #Fókusz levélete az adott widget-ről
    event.widget.destroy()
 
 def enter_pressed(event, table, selected_values):
